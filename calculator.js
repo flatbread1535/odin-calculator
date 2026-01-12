efbb27fce82855d6b609152cac627253098a6734
Adam <ajlarson0731@gmail.com>
2026-01-12 17:51:29 -0500

    Completed operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,13 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        case "-": return subtract(num1, num2);
+        case "*": return multiply(num1, num2);
+        case "/": return divide(num1, num2);
+        default: return undefined;
+    }
+}
